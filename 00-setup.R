ddfe5d6b4d91a5653dbc0a601f8fd7469a3b3c98
morganharris1315 <morganharris1315@gmail.com>
2025-08-08 13:39:57 +1200

    Create 00-setup.R

---
+++ 00-setup.R
@@ -0,0 +1,26 @@
+# _________________________________________________________________________
+# 00-setup.R
+# _________________________________________________________________________
+# August 2025
+# Setup script: clear environment, set working directory, load librabires 
+# _________________________________________________________________________
+
+#rm(list = ls())
+
+setwd("C:/Users/morga/OneDrive - The University of Waikato/Directed Study")
+getwd()
+
+# Load packages
+library(tidyverse)     # Includes ggplot2, dplyr, tidyr, readr, purrr, tibble, stringr, forcats
+library(readxl)        # Reading Excel files
+library(janitor)       # Cleaning column names etc.
+library(scales)        # Formatting axes in plots
+library(skimr)         # Summary statistics
+library(patchwork)     # Combining ggplot plots
+library(glue)          # Interpolated strings
+library(lubridate)     # Dates 
+library(slider)        # Rolling window operations
+library(cowplot) 
+library(dplyr)
+library(grid)
+library(magick)        # GIF Maker
